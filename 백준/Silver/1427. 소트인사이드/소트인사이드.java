c0ad82aed47b8b0d955f9f858dcd0e566a7f01d2
윤성헌 <76799034+Len-Yoon@users.noreply.github.com>
2024-10-10 23:43:24 +0900

    [Silver V] Title: 소트인사이드, Time: 168 ms, Memory: 17780 KB -BaekjoonHub

---
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+
+        char[] arr = sc.nextLine().toCharArray();
+
+        Arrays.sort(arr);
+
+        for (int i = arr.length - 1; i >= 0; i--) {
+            System.out.print(arr[i]);
+        }
+    }
+}
